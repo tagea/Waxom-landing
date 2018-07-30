2484eb29e54d018e306ab35b6b112e91aae215cc
Yulya Vasilyeva <043@tut.by>
2018-07-30 14:57:32 +0300

    Изменение анимации перехода слайдера

--- js/swiper-controls.js
+++ js/swiper-controls.js
@@ -1,6 +1,8 @@
 var swiper = new Swiper('.swiper-container', {
       slidesPerView: 1,
-      spaceBetween: 30,
+      spaceBetween: 10,
+      speed: 400,
+      effect: 'coverflow',
       loop: true,
       pagination: {
         el: '.swiper-pagination',
@@ -9,5 +11,10 @@ var swiper = new Swiper('.swiper-container', {
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+      },
+      coverflowEffect: {
+        rotate: 20,
+        slideShadows: false,
+        modifier: 2,
       },
     });
